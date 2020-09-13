0e7789045ff66ab324a6a8bea6e2f4ae9a760b71
jordan dykstra <jtdykstra123@gmail.com>
2020-09-12 19:17:45 -0700

    add clear button

--- src.js
+++ src.js
@@ -1,8 +1,16 @@
 const GRID_SIZE = 64;
 console.log("starting loop!");
 
+// todo: pull style out of this file
+mainElement = document.querySelector("#content");
+clearBtn = document.createElement('button');
+clearBtn.innerText = "Clear";
+clearBtn.style.width = '5vw';
+clearBtn.style.margin = '5vh';
+mainElement.appendChild(clearBtn);
+
 for (let outerGridInd = 0; outerGridInd < GRID_SIZE*GRID_SIZE; ++outerGridInd) {
-    mainElement = document.querySelector("#content");
+    mainElement = document.querySelector("#pad");
     newElement = document.createElement('div');
     newElement.classList.add('grid-node');
     newElement.style.backgroundColor = 'white';
@@ -21,5 +29,16 @@ gridNodes.forEach((n) => {
 function colorSquare(e) {
     console.log(e);
     e.srcElement.style.backgroundColor = 'black';
+}
+
+clearBtn.addEventListener('click', clearColoring);
+
+function clearColoring(e) {
+    
+    gridNodes = document.querySelectorAll('.grid-node');
+
+    [...gridNodes].forEach((node) => {
+        node.style.backgroundColor = 'white';
+    });
 }
 
