307fd99ed44cb3ae1e4d2e7d0e3bbb862871ac37
jordan dykstra <jtdykstra123@gmail.com>
2020-09-16 20:37:55 -0700

    add key movement

--- src.js
+++ src.js
@@ -1,22 +1,20 @@
 const GRID_SIZE = 64;
 console.log("starting loop!");
 
-// todo: pull style out of this file
+let coordX = 0;
+let coordY = GRID_SIZE - 1;
+
 mainElement = document.querySelector("#content");
 clearBtn = document.createElement('button');
+clearBtn.id = 'clear-btn';
 clearBtn.innerText = "Clear";
-clearBtn.style.width = '5vw';
-clearBtn.style.margin = '5vh';
 mainElement.appendChild(clearBtn);
 
 for (let outerGridInd = 0; outerGridInd < GRID_SIZE*GRID_SIZE; ++outerGridInd) {
     mainElement = document.querySelector("#pad");
     newElement = document.createElement('div');
     newElement.classList.add('grid-node');
-    newElement.style.backgroundColor = 'white';
-    newElement.style.borderColor = '#f0e6e6';
-    newElement.style.borderWidth = 'thin';
-    newElement.style.borderStyle = 'solid';
+    newElement.id = `grid-node-x${outerGridInd % GRID_SIZE}y${Math.floor(GRID_SIZE - (outerGridInd / GRID_SIZE))}`;
     mainElement.appendChild(newElement);
 }
 
@@ -25,6 +23,65 @@ let gridNodes = document.querySelectorAll('.grid-node');
 gridNodes.forEach((n) => {
     n.addEventListener('mouseenter', colorSquare);
 });
+
+window.addEventListener('keydown', keyDownHandler);
+window.addEventListener('keyup', keyUpHandler);
+
+let keyHandlers = {68: () => { move(1, 0) }, 
+                   87: () => { move(0, 1) },
+                   83: () => { move(0, -1) },
+                   65: () => { move(-1, 0) }};
+let timers = {};
+const REPEAT_INTERVAL = 50;
+
+function move(x, y) {
+    coordX = coordX + x;
+    coordY = coordY + y;
+
+    if (coordX < 0) {
+        coordX = 0;
+    }
+    if (coordY < 0) {
+        coordY = 0;
+    }
+    if (coordX > GRID_SIZE) {
+        coordX = GRID_SIZE;
+    }
+    if (coordY > GRID_SIZE) {
+        coordY = GRID_SIZE;
+    }
+    console.log("x " + coordX);
+    console.log("y " + coordY);
+
+    query = `#grid-node-x${coordX}y${coordY}`;
+    console.log("query is " + query);
+    square = document.querySelector(query);
+    console.log(square);
+
+    if (square != null) {
+        square.style.backgroundColor = 'black';
+    }
+}
+
+function keyDownHandler(e) {
+    console.log("key down " + e.keyCode);
+
+    if (!(e.keyCode in keyHandlers))
+        return true;
+    if (!(e.keyCode in timers)) {
+        timers[e.keyCode] = null;
+        keyHandlers[e.keyCode]();
+        timers[e.keyCode] = setInterval(keyHandlers[e.keyCode], REPEAT_INTERVAL);
+    }
+}
+
+function keyUpHandler(e) {
+    console.log("key up " + e.keyCode);
+    if (e.keyCode in timers) {
+        clearInterval(timers[e.keyCode]);
+        delete timers[e.keyCode];
+    }
+}
 
 function colorSquare(e) {
     console.log(e);
